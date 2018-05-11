cea7b989a166e17e9af8efac57256790ddbf20d5
SumGuyTwitches <39174293+SumGuyTwitches@users.noreply.github.com>
2018-05-11 02:59:31 -0500

    Initial commit

---
+++ FuelMyShip/FuelMyShip.cs
@@ -0,0 +1,55 @@
+﻿
+using UnityEngine;
+
+namespace FuelMyShip
+{
+    //might want a vessel module?
+
+    [KSPAddon(KSPAddon.Startup.Flight, true)]
+    public class FuelMyShip : MonoBehaviour
+    {
+
+        public static void Log(string message)
+        {
+            Debug.Log("FuelMyShipLog -------------------- " + ":" + message);
+        }
+        
+        public void Start()
+        {
+            if (HighLogic.LoadedSceneIsFlight)
+            {
+
+                for (var i = 0; i < FlightGlobals.Vessels.Count; i++)
+                {
+                    for (var j = 0; j < FlightGlobals.Vessels[i].parts.Count; j++)
+                    {
+                        Log("   ");
+                        Log(FlightGlobals.Vessels[i].parts[j].name);
+                        for (int k = 0; k < FlightGlobals.Vessels[i].parts[j].Modules.Count; k++)
+                        {
+
+                            if(FlightGlobals.Vessels[i].parts[j].Modules[k].moduleName == "ModuleDockingNode")
+                            {
+
+                                for (int l = 0; l < FlightGlobals.Vessels[i].parts[j].Modules.Count; l++)
+                                {
+                                    Log(FlightGlobals.Vessels[i].parts[j].Modules[l].moduleName);
+                                }
+                                FlightGlobals.Vessels[i].parts[j].AddModule("FuelMyShipModule", false);
+                                
+                                for (int l = 0; l < FlightGlobals.Vessels[i].parts[j].Modules.Count; l++)
+                                {
+                                    Log(FlightGlobals.Vessels[i].parts[j].Modules[l].moduleName);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                
+
+            }
+        }
+        
+    }
+}
---
+++ FuelMyShip/FuelMyShipModule.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+namespace FuelMyShip
+{
+    class FuelMyShipModule : PartModule
+    {
+        [KSPEvent(guiName = "Fill Me Up Scotty", guiActive = true)]
+        public void DoNothing()
+        {
+            //FuelMyShip.Log("Full!");
+        }
+    }
+}
